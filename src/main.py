737d8c5f283debc3d33e95107b5b525c9b1abc61
Bandit0621 <mata.smoldas@centrum.cz>
2025-03-25 11:49:05 +0100

    začátek

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from turtle import * 
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+turt = Turtle("square")
+turt.color("white")
+
+mainloop()
